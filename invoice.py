959cabf36b7ca14863d06e8a429a6c46152001e4
Hanck1215 <st11025212@gmail.com>
2022-12-30 18:28:19 +0800

    新增oled在test檔案中

--- invoice.py
+++ invoice.py
@@ -4,7 +4,6 @@ Created on Wed Dec 28 18:10:00 2022
 
 @author: 88696
 """
-from pynput import keyboard
 
 class InvoiceRedemptionMachine :
         
@@ -28,15 +27,20 @@ class InvoiceRedemptionMachine :
                 return False
     
     database =  DataBase()
+    
     data_posiotion = {}
     cursor = 1 
     index_limit = 5 
        
     Special_Award = ""  #特別獎
     Secondary_Special_Award = ""  #特獎
-    Jackpot_To_SixthPrize = []  #頭獎~六獎
+    Jackpot = ""  #頭獎八位數
+    second_prize = ""  #七位數
+    third_prize = ""  #六位數
+    fourth_prize = ""  #五位數
+    fifth_prize = ""  #四位數
+    sixth_prize = ""  #三位數
     
-    eight = set()
     seven = {}
     six = {}
     five = {}
@@ -44,60 +48,104 @@ class InvoiceRedemptionMachine :
     three = {}
 
 
-    def store(self) :
-        
-        while(True) :
-            number = input("請輸入欲存入的兌獎八碼: ")
+    def store(self, number) :
+        if len(number) != 8 :
+            return 0 #非八位數
+        else :
+            if self.database.search(number) is True :
+                return 1 #此數字已存在
             
-            if number == "-" :
-                print("回上頁")
-                break 
+            self.database.save(number)
             
-            if len(number) != 8 :
-                print("須為八個數字!")
-                continue
+            if self.seven.get(number[1:8]) is None :
+                self.seven.setdefault(number[1:8], [number])
             else :
-                if self.database.search(number) is True :
-                    print("此數字已存在")
-                    continue
+                self.seven.get(number[1:8]).append(number)
+            
+            if self.six.get(number[2:8]) is None :
+                self.six.setdefault(number[2:8], [number])
+            else :
+                self.six.get(number[2:8]).append(number)
                 
-                self.database.save(number)
+            if self.five.get(number[3:8]) is None :
+                self.five.setdefault(number[3:8], [number])
+            else :
+                self.five.get(number[3:8]).append(number)
                 
-                self.eight.add(number)
+            if self.four.get(number[4:8]) is None :
+                self.four.setdefault(number[4:8], [number])
+            else :
+                self.four.get(number[4:8]).append(number)
                 
-                if self.seven.get(number[1:8]) is None :
-                    self.seven.setdefault(number[1:8], [number])
-                else :
-                    self.seven.get(number[1:8]).append(number)
+            if self.three.get(number[5:8]) is None :
+                self.three.setdefault(number[5:8], [number])
+            else :
+                self.three.get(number[5:8]).append(number)
                 
-                if self.six.get(number[2:8]) is None :
-                    self.six.setdefault(number[2:8], [number])
-                else :
-                    self.six.get(number[2:8]).append(number)
-                    
-                if self.five.get(number[3:8]) is None :
-                    self.five.setdefault(number[3:8], [number])
-                else :
-                    self.five.get(number[3:8]).append(number)
-                    
-                if self.four.get(number[4:8]) is None :
-                    self.four.setdefault(number[4:8], [number])
-                else :
-                    self.four.get(number[4:8]).append(number)
-                    
-                if self.three.get(number[5:8]) is None :
-                    self.three.setdefault(number[5:8], [number])
-                else :
-                    self.three.get(number[5:8]).append(number)
-                    
-                self.data_posiotion.setdefault(number, self.cursor)
-                self.cursor += 1
-                if self.cursor == (self.index_limit + 1) :
-                    self.cursor = 1
-                print("儲存成功")
-                continue
+            self.data_posiotion.setdefault(number, self.cursor)
+            self.cursor += 1
+            
+            if self.cursor == (self.index_limit + 1) :
+                self.cursor = 1
+                
+            return 2 #儲存成功
     
-    def redemption(self) :
+    def redemption_Special_Award(self, number) :
+        if self.database.search(number) is True :
+            return self.data_posiotion.get(number)
+        else :
+            return 0
+        
+    def redemption_Secondary_Special_Award(self, number) :
+        if self.database.search(number) is True :
+            return self.data_posiotion.get(number)
+        else :
+            return 0
+        
+    def redemption_Jackpot(self, number) :
+        if self.database.search(number) is True :
+            return self.data_posiotion.get(number)
+        else :
+            return 0
+        
+    def redemption_second_prize(self, number) :
+        seven_num = number[1:8]
+        if self.seven.get(seven_num) is not None :
+            return self.seven.get(seven_num)
+        else :
+            return []
+        
+    def redemption_third_prize(self, number) :
+        six_num = number[2:8]
+        if self.six.get(six_num) is not None :
+            return self.six.get(six_num)
+        else :
+            return []
+        
+    def redemption_fourth_prize(self, number) :
+        five_num = number[3:8]
+        if self.five.get(five_num) is not None :
+            return self.five.get(five_num)
+        else :
+            return []
+        
+    def redemption_fifth_prize(self, number) :
+        four_num = number[4:8]
+        if self.four.get(four_num) is not None :
+            return self.four.get(four_num)
+        else :
+            return []
+        
+    def redemption_sixth_prize(self, number) :
+        three_num = number[5:8]
+        if self.three.get(three_num) is not None :
+            return self.three.get(three_num)
+        else :
+            return []
+        
+
+        
+        
         
         
             
--- test.py
+++ test.py
@@ -6,15 +6,131 @@ Created on Wed Dec 28 18:29:22 2022
 """
 
 import keyboard
+import time
 
-while True:
-    if keyboard.read_key() == "9":
-        print("You pressed p")
-        break
+#keyboard.on_press_key("num lock", lambda _:print("You pressed r"))
 
-while True:
-    if keyboard.is_pressed("r"):
-        print("You pressed q")
-        break
+class oled_displayer :
+    cursor = 1
+    current_page = 1
+    pages = {1:["對獎", "儲存"], 2:["回上頁", "特別獎", "特獎", "頭獎", "二獎", "三獎", "四獎", "五獎", "六獎"], 3:["繼續", "返主頁"]}
+    Input = "請輸入八位數字: "
+        #
+        #
+    def screen_update(self, num = "") :
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+        print(self.current_page, self.cursor)
         
-keyboard.on_press_key("5", lambda _:print("You pressed r"))
+        if self.cursor is False :
+            self.Input = self.Input + num
+            print(self.Input)
+            return True
+        
+        catalog = self.pages.get(self.current_page)
+        for i in range(1, len(catalog)+1) :
+           
+            print(i, ".", catalog[i-1], end = "")
+            if i == self.cursor :
+                print(" <--", end = "")
+            print()
+        
+    def keyboard_up(self) :
+        
+        
+        if self.cursor is False :
+            self.screen_update("8")
+            return True
+        
+        if self.cursor == 1 :
+            self.screen_update()
+            return False
+        else :
+            self.cursor = self.cursor - 1
+            self.screen_update()
+            return True
+        
+    
+    def keyboard_down(self) :
+        
+        if self.cursor is False :
+            self.screen_update("2")
+            return True
+        
+        catalog = self.pages.get(self.current_page)
+        length = len(catalog)
+        
+        if self.cursor == length :
+            self.screen_update()
+            return False
+        else :
+            self.cursor = self.cursor + 1
+            self.screen_update()
+            return True
+        
+        
+    def keyboard_enter(self) :
+        if self.cursor == 1 and self.current_page == 1 :
+            print(100)
+            self.current_page = 2
+            self.screen_update()
+            
+        elif self.cursor == 2 and self.current_page == 1 :
+            self.cursor = False
+            self.current_page = 1
+            self.screen_update()
+            
+            
+        elif self.current_page == 2 and self.cursor == 1 :
+            self.current_page = 1
+            self.screen_update()
+            
+        elif self.current_page == 3 and self.cursor == 1 :
+            self.cursor = False
+            self.current_page = 1
+            self.screen_update()
+            
+        elif self.current_page == 3 and self.cursor == 2 :
+            self.cursor = 1
+            self.current_page = 1
+            self.screen_update()
+        
+        elif self.cursor is False :
+            if len(self.Input) != 17 :
+                self.Input = "請輸入八位數字: 必須是八位數!"
+                self.screen_update()
+                time.sleep(5)
+                self.Input = "請輸入八位數字: "
+                self.screen_update()
+            else :
+                if self.current_page == 1 :
+                    self.Input = "儲存成功!"
+                    self.screen_update()
+                    time.sleep(3)
+                    self.current_page = 3
+                    self.cursor = 1
+                    self.Input = "請輸入八位數字: "
+                    self.screen_update()
+                    
+                    
+        
+        
+
+    def __init__(self) :
+        keyboard.on_press_key("8", lambda _:self.keyboard_up())
+        keyboard.on_press_key("2", lambda _:self.keyboard_down())
+        keyboard.on_press_key("enter", lambda _:self.keyboard_enter())
+        print("歡迎使用發票對獎機!")
+        time.sleep(2)
+        self.screen_update()
+        
+        
+        
+        
+        
+
+
+
+a = oled_displayer()
+
+
+
